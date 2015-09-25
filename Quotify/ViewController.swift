79cfabb964462072a7aa8563bccf8a76c08b2ecf
Moritz <revil4.artwork@gmail.com>
2015-09-25 17:47:53 +0200

    Initial Commit

---
+++ Quotify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quotify
+//
+//  Created by Moritz Gort on 25/09/15.
+//  Copyright © 2015 Gabriele Gort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
